581028a8a63f1051a5b7bda0e6fcb9aa27e7fdc1
rwscherrer <rwscherrer@gmail.com>
2016-08-30 20:08:48 -0500

    nil handler

--- app/controllers/params_games_controller.rb
+++ app/controllers/params_games_controller.rb
@@ -2,24 +2,28 @@ class ParamsGamesController < ApplicationController
 
 
   def name
-    @name = params[:name].upcase
     
-    if @name.chr == 'A'
-      @message = 'Hey, your name starts with the first letter of the alphabet!'
+    if @name != nil
+      @name = params[:name].upcase
+      if @name.chr == 'A'
+        @message = 'Hey, your name starts with the first letter of the alphabet!'
+      end
     end
   end
 
   def numbers
     @number = 37
     @guess = params[:guess]
-    if @guess.to_i == @number
-      @message = 'You guessed correctly!'
-    elsif @guess.to_i < @number
-      @message = 'You guessed too low, guess a higher number'
-    elsif @guess.to_i > @number
-      @message = 'You guessed too high, guess a lower number'
+    if @guess != nil
+      if @guess.to_i == @number
+        @message = 'You guessed correctly!'
+      elsif @guess.to_i < @number
+        @message = 'You guessed too low, guess a higher number'
+      elsif @guess.to_i > @number
+        @message = 'You guessed too high, guess a lower number'
+      end
     end
-      
+    @message = 'You did not guess a number' 
   
   end
 end
