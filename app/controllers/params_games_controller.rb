c11abe49e1fc86822521e160509bfd4c5018081c
rwscherrer <rwscherrer@gmail.com>
2016-08-31 19:12:08 -0500

    added form for guessing game

--- app/controllers/params_games_controller.rb
+++ app/controllers/params_games_controller.rb
@@ -27,4 +27,52 @@ class ParamsGamesController < ApplicationController
   @message = 'You did not guess a number between 1 and 100' 
   
   end
+
+  def url
+    @message = params[:wildcard]
+  end
+
+  def url_guess_number
+    @number = 37
+    @guess = params[:guess]
+
+    if @guess != nil
+      if @guess.to_i == @number
+        @message = 'You guessed correctly!'
+      elsif @guess.to_i < @number
+        @message = 'You guessed too low, guess a higher number'
+      elsif @guess.to_i > @number
+        @message = 'You guessed too high, guess a lower number'
+      end
+    end
+  end
+
+  def get_form
+  
+  end
+  
+ 
+
+  def send_form
+    
+    guess = params[:guess]
+    if guess
+      guess = guess.to_i
+      correct_answer = 32
+
+      if guess == correct_answer
+        @message = "You Did It!!!!"
+      elsif guess < 1 || guess > 100
+        @message = "You're out of bounds, Guy."
+      elsif guess < correct_answer
+        @message = "Guess higher, buddy."
+      elsif guess > correct_answer
+        @message = "Guess lower, friend"
+      end
+    else
+      @message = "You obviously need to put something in the URL."
+    end
+  end
+    
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
 get '/params_games' => 'params_games#game' 
 get '/name' => 'params_games#name'
 get '/numbers' => 'params_games#numbers'
+get '/url_segment_example/:wildcard' => 'params_games#url'
+get '/url_guess_number/:guess' => 'params_games#url_guess_number'
+get '/show_form' => 'params_games#get_form'
+post '/send_form' => 'params_games#send_form'
 end
